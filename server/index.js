51416898ce3dddcef4c665234958f88f295c41f6
arnold1105 <arnold.b.cheung@gmail.com>
2019-02-08 16:55:43 -0800

    create database and seed file to database

--- server/index.js
+++ server/index.js
@@ -4,13 +4,11 @@ let app = express();
 app.use(express.static(__dirname + '/../client/dist'));
 
 app.post('/', function (req, res) {
-  console.log("hello")
-      res.status(201).send();
+  res.status(201).send();
 });
 
 app.get('/', function (req, res) {
-  console.log("heo")
-      res.status(200).send("testing");
+  res.status(200).send('testing');
 });
 
 let port = 3000;
